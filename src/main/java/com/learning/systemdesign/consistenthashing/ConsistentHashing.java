b486f88b4c3e15800818685ff92596cbe29a008f
tulasidamarla <ram.damarla@gmail.com>
2023-08-05 18:08:04 +0530

    ConsistentHashing program

--- src/main/java/com/learning/systemdesign/consistenthashing/ConsistentHashing.java
+++ src/main/java/com/learning/systemdesign/consistenthashing/ConsistentHashing.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class ConsistentHashing {
     private final TreeMap<Long, String> ring;
+    private final int ringSize;
     private final int numberOfReplicas;
     private final MessageDigest md;
 
@@ -13,6 +14,7 @@ public class ConsistentHashing {
         this.ring = new TreeMap<>();
         this.numberOfReplicas = numberOfReplicas;
         this.md = MessageDigest.getInstance("MD5");
+        this.ringSize = 256;
     }
 
     public void addServer(String server) {
@@ -41,6 +43,10 @@ public class ConsistentHashing {
         return ring.get(hash);
     }
 
+    public TreeMap<Long, String> getMap(){
+        return this.ring;
+    }
+
     private long generateHash(String key) {
         md.reset();
         md.update(key.getBytes());
@@ -49,7 +55,7 @@ public class ConsistentHashing {
                 ((long) (digest[2] & 0xFF) << 16) |
                 ((long) (digest[1] & 0xFF) << 8) |
                 ((long) (digest[0] & 0xFF));
-        return hash;
+        return hash % ringSize;
     }
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
@@ -57,15 +63,20 @@ public class ConsistentHashing {
         ch.addServer("server1");
         ch.addServer("server2");
         ch.addServer("server3");
-
+        ch.addServer("server4");
 
         System.out.println("key1: is present on server: " + ch.getServer("key1"));
+        System.out.println("testkey1: is present on server: " + ch.getServer("testkey1"));
+        System.out.println("key100: is present on server: " + ch.getServer("key100"));
+        System.out.println("key5000: is present on server: " + ch.getServer("key5000"));
         System.out.println("key67890: is present on server: " + ch.getServer("key67890"));
 
-        ch.removeServer("server1");
-        System.out.println("After removing server1");
-
+        ch.removeServer("server3");
+        System.out.println("After removing server3");
         System.out.println("key1: is present on server: " + ch.getServer("key1"));
+        System.out.println("testkey1: is present on server: " + ch.getServer("testkey1"));
+        System.out.println("key100: is present on server: " + ch.getServer("key100"));
+        System.out.println("key5000: is present on server: " + ch.getServer("key5000"));
         System.out.println("key67890: is present on server: " + ch.getServer("key67890"));
     }
 }
